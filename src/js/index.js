33c97a7d1a3ba2a9749504ea4641dba9209c49e7
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-08-26 00:50:53 +0000

    John's first commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,15 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+	const one = Math.floor(counter/1);
+	const two = Math.floor(counter/10);
+	const three = Math.floor(counter/100);
+	const four = Math.floor(counter/1000);
+	const five = Math.floor(counter/10000);
+	const six = Math.floor(counter/100000);
+    counter++;
+    ReactDOM.render(<Home digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>, document.querySelector("#app"));
+}, 1000);
+
